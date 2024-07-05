9b3729ef6399b4616d51c6a8baffd0486bfbff29
Victor Jun <107790828+GeroJun@users.noreply.github.com>
2024-07-05 19:35:33 +0900

    Fixing bug

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
-import SearchScreen from "./src/screens/SearchScreen";
-import ResultsShowScreen from "./src/screens/ResultsShowScreen";
+import SearchScreen from './src/screens/SearchScreen';
+import ResultsShowScreen from './src/screens/ResultsShowScreen';
 
-const Stack = createStackNavigator();
+const navigator = createStackNavigator(
+  {
+    Search: SearchScreen,
+    ResultsShow: ResultsShowScreen,
+  },
+  {
+    initialRouteName: 'Search',
+    defaultNavigationOptions: {
+      title: 'Business Search',
+    },
+  }
+);
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerTitle: "Business Search" }}>
-        <Stack.Screen name="Search" component={SearchScreen} />
-        <Stack.Screen name="ResultsShow" component={ResultsShowScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
+export default createAppContainer(navigator);
